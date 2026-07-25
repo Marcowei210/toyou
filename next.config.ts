15bddafe47cc6e30f8ef6e303c8b7e3a9d0184a9
Marco <marco@gmail.com>
2026-07-26 03:08:21 +0800

    fix firebase-admin esm error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.108', 'http://192.168.1.108:3000'],
+  serverExternalPackages: ["firebase-admin"],
 };
 
 export default nextConfig;
